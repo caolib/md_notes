c4217a658053b1fda233b223ccde6d44a3204e49
Bin <1265501579@qq.com>
2024-03-24 07:39:36 +0000

    删除文件 push.py

--- push.py
+++
@@ -1,8 +0,0 @@
-import os
-import time
-
-time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
-
-os.system('git add .')
-os.system(f'git commit -m "{time}"')
-os.system('git push')
